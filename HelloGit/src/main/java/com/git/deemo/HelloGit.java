eb8b5304563b75e08d3469c48bedb3cf02403b16
yoyo-pineapple <sbh12345678>
2021-05-19 21:20:14 +0800

    commit branches

--- HelloGit/src/main/java/com/git/deemo/HelloGit.java
+++ HelloGit/src/main/java/com/git/deemo/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HeeloGit");
         System.out.println("Hello SpriderMan");
+        System.out.println("Set up Branches");
     }
 }
