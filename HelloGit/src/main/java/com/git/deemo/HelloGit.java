ba9380d31a6758a69508232e491aa5ec46d4289c
yoyo-pineapple <sbh12345678>
2021-05-19 21:08:36 +0800

    commit 2.0

--- HelloGit/src/main/java/com/git/deemo/HelloGit.java
+++ HelloGit/src/main/java/com/git/deemo/HelloGit.java
@@ -3,5 +3,6 @@ package com.git.deemo;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HeeloGit");
+        System.out.println("Hello SpriderMan");
     }
 }
